32990195757b5762e0c41bcbcefef008d660afcc
Thomas <thomas.framery@2021.icam.fr>
2024-08-03 17:24:40 +0200

    model Lightning format

--- src/modelling/gnn.py
+++ src/modelling/gnn.py
@@ -1,14 +1,16 @@
 import torch
 import torch.nn.functional as F
 from torch_geometric.nn import GCNConv, global_mean_pool
+from pytorch_lightning import LightningModule 
 
-class GNNModel(torch.nn.Module):
+class GNNModel(LightningModule):
     def __init__(self):
         super(GNNModel, self).__init__()
         self.conv1 = GCNConv(2, 16)  # 2 input features (x, y) and 16 output features
         self.conv2 = GCNConv(16, 32)
         self.fc1 = torch.nn.Linear(32, 16)
         self.fc2 = torch.nn.Linear(16, 1)
+        self.criterion = torch.nn.MSELoss()
 
     def forward(self, data):
         x, edge_index, batch = data.x, data.edge_index, data.batch
@@ -25,7 +27,23 @@ class GNNModel(torch.nn.Module):
         
         return x.squeeze()
 
+    def training_step(self, batch, batch_idx):
+        output = self(batch)
+        loss = self.criterion(output, batch.y)
+        self.log('train_loss', loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        output = self(batch)
+        loss = self.criterion(output, batch.y)
+        self.log('val_loss', loss)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=0.01)
+        return optimizer
+
+
 if __name__=='__main__':
     model = GNNModel()
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
-    criterion = torch.nn.MSELoss()
+
