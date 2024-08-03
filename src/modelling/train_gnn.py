787d4c7c13d1521ead61f6edb26298ad8fb3b270
Thomas <thomas.framery@2021.icam.fr>
2024-08-03 17:44:18 +0200

    add main for //

--- src/modelling/train_gnn.py
+++ src/modelling/train_gnn.py
@@ -7,22 +7,24 @@ from pytorch_lightning import Trainer
 import wandb
 from pytorch_lightning.loggers import WandbLogger
 
-# Initialize W&B
-wandb.init(project='football-xg')
 
-# Create a W&B logger
-wandb_logger = WandbLogger()
+if __name__=='__main__':
+    # Initialize W&B
+    wandb.init(project='football-xg')
 
-
-graphs = read_data(data_path=PROCESSED_DATA_PATH)
-
-# Split the data into training and testing sets
-train_graphs, test_graphs = train_test_split(graphs, test_size=0.2, random_state=42)
-data_module = GraphDataModule(train_graphs, test_graphs, batch_size=BATCH_SIZE)
-
-model = GNNModel()
+    # Create a W&B logger
+    wandb_logger = WandbLogger()
 
 
+    graphs = read_data(data_path=PROCESSED_DATA_PATH)
 
-trainer = Trainer(max_epochs=200, logger=wandb_logger)
-trainer.fit(model, data_module)
+    # Split the data into training and testing sets
+    train_graphs, test_graphs = train_test_split(graphs, test_size=0.2, random_state=42)
+    data_module = GraphDataModule(train_graphs, test_graphs, batch_size=BATCH_SIZE)
+
+    model = GNNModel()
+
+
+
+    trainer = Trainer(max_epochs=200, logger=wandb_logger)
+    trainer.fit(model, data_module)
