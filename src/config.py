e80a9fc2e9b9518d6574c62883649fe78b048e0c
Thomas <thomas.framery@2021.icam.fr>
2024-08-03 17:18:57 +0200

    setup and global var files

---
+++ src/config.py
@@ -0,0 +1,6 @@
+import os
+
+
+RAW_DATA_PATH = os.path.join('data', 'raw', 'freekick_pass_shot.csv')
+PROCESSED_DATA_PATH = os.path.join('data', 'processed', 'processed_freekick_pass_shot.pkl')
+BATCH_SIZE = 32
